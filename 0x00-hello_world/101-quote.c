a4e44b9733b737affeeb7d19242dcd4e313ba4bb
Berna-Ciru <bernadettekabiru@gmail.com>
2023-02-16 18:50:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", 59);
 	return (1);
 }
