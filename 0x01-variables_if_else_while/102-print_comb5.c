fd6efdd371c5ed727e89c1d04694d952b401d215
Berna-Ciru <bernadettekabiru@gmail.com>
2023-02-17 06:43:46 -0800

    print comb 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int r, m;
 
-	for (r = '0'; r <= '98'; r++)
+	for (r = '0'; r <= 98; r++)
 	{
-		for (m = r + 1; m <= '99'; m++)
+		for (m = r + 1; m <= 99; m++)
 		{
 			putchar((r / 10) + '0');
-			putchar((r % 10) = '0');
+			putchar((r % 10) + '0');
 			putchar(' ');
 			putchar((m / 10) + '0');
 			putchar((m % 10) + '0');
-			if (r == '98' && m == '99')
+			if (r == 98 && m == 99)
 				continue;
 			putchar(',');
 			putchar(' ');
