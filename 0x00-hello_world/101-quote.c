8e2e2f4e13ca16184c8b9ba4c11c0a38bafbc017
Berna-Ciru <bernadettekabiru@gmail.com>
2023-02-16 18:33:20 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unlstd.h>
+#include <unistd.h>
 /**
  * main - a prtinting c program. this is is exciting
  * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
