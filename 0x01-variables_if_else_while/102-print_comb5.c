12d28f691bdaa0b5e145d81b135d087b994150e0
Berna-Ciru <bernadettekabiru@gmail.com>
2023-02-17 06:40:58 -0800

    print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r, m;
+
+	for (r = '0'; r <= '98'; r++)
+	{
+		for (m = r + 1; m <= '99'; m++)
+		{
+			putchar((r / 10) + '0');
+			putchar((r % 10) = '0');
+			putchar(' ');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			if (r == '98' && m == '99')
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
