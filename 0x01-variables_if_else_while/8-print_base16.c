fb14647ce8466a01b0b24b312d3f12eb9d92939d
Berna-Ciru <bernadettekabiru@gmail.com>
2023-02-17 06:00:12 -0800

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return; Always 0
+ */
+int main(void)
+{
+	int a;
+	char toy;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	for (toy = 'a'; toy <= 'f'; toy++)
+		putchar(toy);
+	putchar('\n');
+
+	return (0);
+}
