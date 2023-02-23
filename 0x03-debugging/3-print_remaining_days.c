02158fe96b55fe2d8d0d4a3b69ae94f4b9093206
Berna-Ciru <bernadettekabiru@gmail.com>
2023-02-23 04:40:46 -0800

    print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,7 +20,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
-		else
+	}
+	else
 		{
 			if (month == 2 && day == 60)
 			{
@@ -32,5 +33,4 @@ void print_remaining_days(int month, int day, int year)
 				printf("Remaining days: %d\n", 365 - day);
 			}
 		}
-	}
 }
