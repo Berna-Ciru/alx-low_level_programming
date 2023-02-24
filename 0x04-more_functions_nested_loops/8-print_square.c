e0d37890c867edc19824e825687a2227cc120506
Berna-Ciru <bernadettekabiru@gmail.com>
2023-02-24 06:34:07 -0800

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square on the terminal
+ * @size: the number of squares or the number of times
+ * Return: empty
+ */
+
+void prtin_square(int size)
+{
+	int g, h;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (g = 0; g <= size; g++)
+		{
+			for (h = 0; h <= size; h++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
