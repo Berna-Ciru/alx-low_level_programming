d804f759a606b2f49bf604053849eccea2807942
Berna-Ciru <bernadettekabiru@gmail.com>
2023-03-10 05:19:49 -0800

    new file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prtins name of the program
+ * @argv: the argument vector
+ * @argc: the argument count
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
